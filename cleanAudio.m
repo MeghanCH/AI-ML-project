d8f06e2bff755b9e21cb829fd9051ac3f0b8cc81
Ryler Hockenbury <ryler.hockenbury@gmail.com>
2014-04-17 04:54:52 -0400

    added box filter for audio

--- cleanAudio.m
+++ cleanAudio.m
@@ -5,32 +5,45 @@
 function [cleanData] = cleanAudio(inputData)
 % clean the audio frames by windowing
 
-n = size(inputData, 2); 
+n = size(inputData, 1);
 
 % find the peak and the associated index
-peak = max(inputData); 
-peakIndex = find(inputData == peak); 
+peak = max(inputData);
+peakIndex = find(inputData == peak);
 
-peakIndexSize = size(peakIndex,2); 
+peakIndexSize = size(peakIndex,2);
 
 if(peakIndexSize > 1)
     peakIndex = floor(mean(peakIndex));
 end
 
-window = 8000; 
-audioData = zeros(1,window); 
+% set up a box filter
+width = 4000; 
+filterLength = 24000; 
+audioData = zeros(1,width); 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% finish this processing function
+filter = zeros(1,filterLength);
+filter(filterLength/2 - width/2:filterLength/2 + width/2) = 1;
 
-% 
-% if(floor(peakIndex-window/2) < 1)
-% 
-% audioData = inputData(peakIndex-window/2:peakIndex+window/2)
-% 
-% end
+% align center of box with signal peak
+shift = floor(filterLength/2 - peakIndex);
+filter = circshift(filter', -shift);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+inputData(n:filterLength) = 0; 
+temp = filter.*inputData; 
+audioData = temp(filterLength/2 - shift - width/2: filterLength/2 - shift + width/2); 
+
+% visualize filter
+% subplot(2,1,1)
+% plot(filter,'r');
+% hold on
+% plot(inputData,'b'); 
+% 
+% subplot(2,1,2)
+% plot(audioData); 
+% 
+% pause(2)
+% clf
 
 cleanData = audioData(:); % flatten
 
